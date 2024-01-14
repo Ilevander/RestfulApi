0f3c4cff88f3adea88dbe421d429fd2a6f66f232
dell <dell@DESKTOP-4P8U8AR>
2024-01-14 01:29:48 +0100

    Add Authentication System Using JWT & BCrypt Registration & Login functionnaities

--- RestfulApi/Models/User.cs
+++ RestfulApi/Models/User.cs
@@ -7,8 +7,8 @@ namespace RestfulApi.Models
     {
         public int Id { get; set; }
         public int? RoleId { get; set; }
-        public string? Username { get; set; }
-        public string? PasswordHash { get; set; }
+        public string? Username { get; set; } = string.Empty;
+        public string? PasswordHash { get; set; } = string.Empty;
         public string? Email { get; set; }
         public DateTime? Date { get; set; }
         public string? Address { get; set; }
